4c42199cf383b9dfb11b66440053e9a4db50f6f9
webdevfarid <126426724+webdevfarid@users.noreply.github.com>
2024-02-15 01:38:47 +0400

    Delete HomeWork4 directory

--- HomeWork4/src/main/java/DayOfWeek.java
+++
@@ -1,7 +0,0 @@
-public enum DayOfWeek
-{
-    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
-    public boolean isWeekend() {
-        return this == SATURDAY || this == SUNDAY;
-    }
-}
