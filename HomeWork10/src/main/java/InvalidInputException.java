ab84b2da4a0c6d5d7eb6093df7b597d970e5410d
webdevfarid <126426724+webdevfarid@users.noreply.github.com>
2024-02-27 11:43:41 +0400

    Add files via upload

---
+++ HomeWork10/src/main/java/InvalidInputException.java
@@ -0,0 +1,12 @@
+public class InvalidInputException  extends  Exception
+{
+    private  String message;
+    public InvalidInputException(String message)
+    {
+        this.message=message;
+    }
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ HomeWork10/src/main/java/Main.java
@@ -0,0 +1,90 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("-----first task------");
+        firstTask();
+        System.out.println("-----second task------");
+        secondTask();
+    }
+    static void firstTask()
+    {
+        System.out.println("write down an integer-");
+        Scanner scanner=new Scanner(System.in);
+        while (true)
+        {
+            try
+            {
+                if (scanner.hasNextInt())
+                {
+                    int input= scanner.nextInt();
+                    System.out.println("your number is -"+ input);
+                    if (isInteger(input))
+                    {
+                        System.out.println("it's okay");
+                        break;
+                    }
+//                    else
+//                    {
+//                        throw new InvalidInputException("this is not integer,try again T_T");
+//                    }
+                }
+                else
+                {
+                    throw new InvalidInputException("this is not integer,try again T_T");
+                }
+            }
+            catch (InvalidInputException e)
+            {
+                System.out.println(e.getMessage());
+                scanner.next();
+            }
+
+        }
+
+    }
+
+    static boolean isInteger(int num) throws InvalidInputException
+    {
+        return num%1==0;
+    }
+
+    static void secondTask()
+    {
+        int min=1;
+        int max=10;
+        System.out.println("["+min+":"+max+"]");
+        System.out.println("it will work until you find right number");
+        while(true)
+        {
+            try
+            {
+                Scanner scanner = new Scanner(System.in);
+                int userInput = scanner.nextInt();
+                if (userInput < min || userInput > max)
+                {
+                    throw new InvalidInputException("this number is out of bounds");
+                }
+                else
+                {
+                    System.out.println("it's okay");
+                    break;
+                }
+            }
+            catch (InputMismatchException e1)
+            {
+                System.out.println("this is not even a number T_T");
+            }
+            catch (InvalidInputException e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+        }
+
+
+}
